c286519b5f86e052d6862899173dcb2b76fd2984
Your Name <you@example.com>
2021-02-17 21:25:20 +0000

    Fix version

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    BaseModel class
+    BaseModel class models.storage.new(self)
 """
 from datetime import datetime
 import models
@@ -22,7 +22,7 @@ class BaseModel():
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        models.storage.new(self)
+
 
     def __str__(self):
         """ Print name and other info """

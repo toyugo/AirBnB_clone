a375e0e9e400671e16b71a446211b96bdfc28f50
Your Name <you@example.com>
2021-02-18 21:35:39 +0000

    Add update function

--- console.py
+++ console.py
@@ -93,5 +93,31 @@ class HBNBCommand(cmd.Cmd):
                 if tmp[0] == item_arg[0]:
                     listAll.append(str(StorageAll[k]))
             print(listAll)
+
+    def do_update(self, arg):
+        """
+        Update an instance base on class name id
+        Usage: update <class name> <id> <attribute name> "<attribute value>"
+        """
+        item_arg = arg.split()
+        StorageAll = storage.all()
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif item_arg[0] not in self.class_list:
+            print("** class doesn't exist **")
+        elif len(item_arg) < 2:
+            print("** instance id missing **")
+        elif (item_arg[0] + "." + item_arg[1]) not in StorageAll:
+            print("** no instance found **")
+        elif len(item_arg) < 3:
+            print("** attribute name missing **")
+        elif len(item_arg) < 4:
+            print("** value missing **")
+        else:
+            key = item_arg[0] + "." + item_arg[1]
+            print(StorageAll[key])
+            print(type(StorageAll[key]))
+            setattr(StorageAll[key], item_arg[2], item_arg[3])
+            storage.save()
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
